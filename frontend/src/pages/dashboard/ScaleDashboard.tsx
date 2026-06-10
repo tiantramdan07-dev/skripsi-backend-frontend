b57f3d4a03ef0ec681751d70bb8bb29a0bd74379
Yannn <tiantramdan07@gmail.com>
2026-06-10 13:46:11 +0700

    fix: tambah feed struk RawBT

--- frontend/src/pages/dashboard/ScaleDashboard.tsx
+++ frontend/src/pages/dashboard/ScaleDashboard.tsx
@@ -48,10 +48,7 @@ async function triggerRawBTPrint(
     struk += garis + '\n'
     struk += `Total : ${formatRp(total_harga)}\n`
     struk += garis + '\n'
-    // struk += center('TERIMA KASIH') + '\n'
-    // struk += '\n'
-    // struk += barisWaktu + '\n'
-    // struk += '\n\n'
+    struk += '\n\n\n\n\n'
 
     window.location.href = `rawbt:${encodeURIComponent(struk)}`
 
